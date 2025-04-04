556ec1e1d85bda239047e0b17e434e7f9831b98a
MorrisLin <aka611511@gmail.com>
2025-04-04 23:05:37 +0800

    feat: fix provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 
 import { Analytics } from '@vercel/analytics/react';
+import MiniKitProvider from '@/lib/minikit-provider';
 
 export const metadata = {
   title: 'Next.js App Router + NextAuth + Tailwind CSS',
@@ -8,15 +9,12 @@ export const metadata = {
     'A user admin dashboard configured with Next.js, Postgres, NextAuth, Tailwind CSS, TypeScript, and Prettier.'
 };
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="flex min-h-screen w-full flex-col">{children}</body>
-      <Analytics />
+      <MiniKitProvider>
+        <body className="flex min-h-screen w-full flex-col">{children}</body>
+      </MiniKitProvider>
     </html>
   );
 }
