0c0e162d782f584192211b18b8c0796f97ad90d7
Evis Chang <evis.chang@woo.network>
2025-04-05 02:09:00 +0800

    add send usdc function

--- lib/circle/walletService.ts
+++ lib/circle/walletService.ts
@@ -4,28 +4,31 @@ import {
   WebAuthnMode,
   toModularTransport,
   toCircleSmartAccount,
+  encodeTransfer,
 } from '@circle-fin/modular-wallets-core';
-import { createPublicClient, Transport, Client } from 'viem';
-import { toWebAuthnAccount } from 'viem/account-abstraction';
+import { createPublicClient, Transport, Client, parseEther } from 'viem';
+import { createBundlerClient, SmartAccount, toWebAuthnAccount } from 'viem/account-abstraction';
 
 // testing chains
 import { baseSepolia, arbitrumSepolia, sepolia } from 'viem/chains';
 
+const clientKey = process.env.CIRCLE_CLIENT_KEY || '';
+const clientUrl = process.env.CIRCLE_CLIENT_URL || ''; 
+const chainName = process.env.CIRCLE_CHAIN_NAME || '';
+const USDC_CONTRACT_ADDRESS = process.env.USDC_CONTRACT_ADDRESS as `0x${string}` || '0x0000000000000000000000000000000000000000';
+const USDC_DECIMALS = process.env.USDC_DECIMALS || 6;
+const NUMBER_OF_USDC = 10 ** Number(USDC_DECIMALS);
+
 export class walletService {
   private publicClient: any = null;
   private modularTransport: any = null;
-  private readonly clientKey: string;
-  private readonly clientUrl: string; 
-  private readonly chainName: string;
+
   private passkeyTransport: any = null;
 
   constructor() {
-    this.clientKey = process.env.CIRCLE_CLIENT_KEY || '';
-    this.clientUrl = process.env.CIRCLE_CLIENT_URL || '';
-    this.chainName = process.env.CIRCLE_CHAIN_NAME || '';
     this.modularTransport = toModularTransport(
-      `${this.clientUrl}/${this.chainName}`,
-      this.clientKey
+      `${clientUrl}/${chainName}`,
+      clientKey
     );
     this.publicClient = createPublicClient({
       chain: arbitrumSepolia,
@@ -35,7 +38,7 @@ export class walletService {
   }
 
   /**
-   * Get a credential for a wallet by World ID
+   * Get a credential for a wallet by World ID (Registration)
    * @param hashWorldId - The hash of the World ID
    * @returns The credential for the passkey
    */
@@ -58,6 +61,12 @@ export class walletService {
     }
   }
 
+  /**
+   * Get a credential for a wallet by World ID (Login)
+   * @param hashWorldId - The hash of the World ID
+   * @param credentialId - Existed passkey
+   * @returns The credential for the passkey
+   */
   async getCredentialByWorldIdForLogin(hashWorldId?: string, credentialId?: string) {
     if (!hashWorldId && !credentialId) {
       throw new Error('hashWorldId or credentialId is required');
@@ -91,6 +100,7 @@ export class walletService {
 
   /**
    * 1. Get Crendential first then initialize Smart Account
+   * 2. Initialize Smart Account
    * @param credential - The credential for the passkey
    * @returns The smart account
    */
@@ -111,5 +121,60 @@ export class walletService {
       console.error('INITIALIZE_SMART_ACCOUNT', error);
       throw error;
     }
+  }
+
+  /**
+   * Send USDC transfer (For testing purposes) based on the smart account
+   * @param recipientAddress - The recipient address
+   * @param amount - The amount of USDC to send
+   * @param smartAccount - The smart account
+   * @returns The user operation hash
+   */
+  async sendUSDCTransfer(recipientAddress: string, amount: string, smartAccount: any) {
+    try {
+      console.log('SEND_USDC_TRANSFER', { recipientAddress, amount: amount.toString() });
+
+      if (!this.publicClient || !smartAccount) {
+        throw new Error('Smart account not initialized');
+      }
+
+      const bundlerClient = createBundlerClient({
+          account: smartAccount as SmartAccount,
+          chain: arbitrumSepolia,
+          transport: this.modularTransport,
+      })
+
+      const userOpHash = await bundlerClient.sendUserOperation({
+        calls: [encodeTransfer(recipientAddress as `0x${string}`, USDC_CONTRACT_ADDRESS, this.parseToBigInt(amount, 'USDC'))],
+        account: smartAccount as SmartAccount, 
+        paymaster: true
+      });
+
+      console.log('userOpHash', userOpHash);
+
+      const receipt = await bundlerClient.waitForUserOperationReceipt({
+        hash: userOpHash
+      });
+
+      console.log('SEND_USDC_TRANSFER', { userOpHash });
+      return receipt.userOpHash;
+    } catch (error) {
+      console.error('SEND_USDC_TRANSFER', error);
+      throw error;
+    }
+  }
+
+  private parseToBigInt(amount: string, currency: string): bigint {
+    if (currency === 'USDC') {
+      // Convert 0.001 USDC to its smallest unit (microUSDC)
+      // USDC has 6 decimal places, so multiply by 10^6
+      const amountInSmallestUnit = Math.floor(parseFloat(amount) * 1_000_000);
+      // Now convert to BigInt
+      return BigInt(amountInSmallestUnit);
+    } else if (currency === 'ETH') {
+      return parseEther(amount);
+    } 
+
+    throw new Error('Invalid currency');
   }
 } 
