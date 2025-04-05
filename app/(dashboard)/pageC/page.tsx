94e332b9e71a21ae307177b84b699e7f076c7449
Eddie Hsu <aoe102198@gmail.com>
2025-04-06 02:43:57 +0800

    Test to integrate ApplePayButton

--- app/(dashboard)/pageC/page.tsx
+++ app/(dashboard)/pageC/page.tsx
@@ -1,14 +1,17 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import ApplePayButton from '@/components/applePay/ApplePay';
 
 export default function PageC() {
-  //
   return (
     <Card>
       <CardHeader>
         <CardTitle>PageC</CardTitle>
         <CardDescription>View PageC</CardDescription>
       </CardHeader>
-      <CardContent></CardContent>
+      <CardContent>
+        {/* 使用 ApplePayButton 元件 */}
+        <ApplePayButton amount="27.50" label="Demo (Card is not charged)" />
+      </CardContent>
     </Card>
   );
 }
